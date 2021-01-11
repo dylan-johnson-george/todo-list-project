64aeadba4eeb4e5063448412a50ebb3fa28b25fd
Dylan Johnson <dylan.johnson@holidayextras.com>
2021-01-11 13:52:05 +0000

    remove unnecessary cors code and change order of routes

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ require('./services/passport')
 const path = require('path')
 
 const app = express()
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, 'client/build')));
+//   app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//   });
+// }
 
 const port = process.env.PORT || 4000
 
@@ -30,23 +30,6 @@ app.use(cors({
   credentials: true,
   origin: 'http://localhost:3000'
 }))
-
-
-// const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://shrouded-journey-38552.heroku']
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     console.log("** Origin of request " + origin)
-//     if (whitelist.indexOf(origin) !== -1 || !origin) {
-//       console.log("Origin acceptable")
-//       callback(null, true)
-//     } else {
-//       console.log("Origin rejected")
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
-// app.use(cors(corsOptions))
-
 
 
 app.use(bodyParser.urlencoded({
@@ -67,6 +50,13 @@ app.use(passport.session())
 
 app.use(todoRoutes)
 app.use(authRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log('Listening on port 4000')
