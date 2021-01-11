4020074e1085496228bc9033eaf188d28ac2efa6
Dylan Johnson <dylan.johnson@holidayextras.com>
2021-01-11 10:10:35 +0000

    update paths

--- app.js
+++ app.js
@@ -11,11 +11,10 @@ require('./services/passport')
 const path = require('path')
 
 const app = express()
-
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '..client/public')));
+  app.use(express.static(path.join(__dirname, '/client/public')));
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
+    res.sendFile(path.join(__dirname, '/client/public', 'index.html'));
   });
 }
 
