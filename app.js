63d543023ef90d2d90f338ce8c7595653e663443
Dylan Johnson <dylan.johnson@holidayextras.com>
2021-01-11 09:36:37 +0000

    add environment variable and server react files

--- app.js
+++ app.js
@@ -9,9 +9,17 @@ const authRoutes = require('./routes/authRoutes')
 require('dotenv').config()
 require('./services/passport')
 const clientDomain = require('./utils.js/clientDomain')
-
+const path = require('path')
 
 const app = express()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 4000
 
 
@@ -24,6 +32,24 @@ app.use(cors({
   credentials: true,
   origin: clientDomain
 }))
+
+
+// const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://shrouded-journey-38552.heroku']
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     console.log("** Origin of request " + origin)
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       console.log("Origin acceptable")
+//       callback(null, true)
+//     } else {
+//       console.log("Origin rejected")
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+// app.use(cors(corsOptions))
+
+
 
 app.use(bodyParser.urlencoded({
   extended: true
