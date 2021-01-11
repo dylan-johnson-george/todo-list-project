54305f9c09e9b963f3d3cda7a750c6c7972065a5
Dylan Johnson <dylan.johnson@holidayextras.com>
2021-01-11 10:41:24 +0000

    test removing send function

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ const path = require('path')
 const app = express()
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+  // app.get('*', function(req, res) {
+  //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  // });
 }
 
 const port = process.env.PORT || 4000
